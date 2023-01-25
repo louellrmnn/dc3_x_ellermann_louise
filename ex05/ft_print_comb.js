95b409a1216a206122763e79b0303b70f93adf4b
louellrmnn <ellermannlouise@gmail.com>
2023-01-25 13:22:21 +0100

    MAJ ex

--- ex05/ft_print_comb.js
+++ ex05/ft_print_comb.js
@@ -1,4 +1,4 @@
-function printComb () {
+function printComb() {
     for (let a = 0; a <= '9'; a++) {
         for (let b = a + 1; b <= '9'; b++) {
             for (let c = b + 1; c <= '9'; c++) {
@@ -7,5 +7,3 @@ function printComb () {
         }
     }
 }
-
-
